9e52e29ec06d739566c7b75daf422e7364bcb673
nk <nokla133711@gmail.com>
2022-11-14 23:55:00 +0300

    update

--- 9lab/WebDriverTests/WebDriverTests/PastebinTests.cs
+++ 9lab/WebDriverTests/WebDriverTests/PastebinTests.cs
@@ -27,35 +27,43 @@ namespace FunctionalTests
         [Test]
         public void BringItOn()
         {
-            IWebDriver driver = new ChromeDriver();
-
+            WebDriver driver = new ChromeDriver();
             driver.Url = "https://pastebin.com";
-
-            driver.FindElement(By.Id("postform-text")).SendKeys("git config--global user.name  \"New Sheriff in Town\" \ngit reset $(git commit - tree HEAD ^{ tree} -m \"Legacy code\") \ngit push origin master --force");
-
+            driver.FindElement(By.Id("postform-text")).SendKeys("git config --global user.name  \"New Sheriff in Town\"" + "\ngit reset $(git commit - tree HEAD ^{ tree} -m \"Legacy code\") " + "\ngit push origin master --force");
             driver.FindElement(By.Id("select2-postform-format-container")).Click();
             driver.FindElement(By.XPath("//li[text()='Bash']")).Click();
-
             driver.FindElement(By.Id("select2-postform-expiration-container")).Click();
             driver.FindElement(By.XPath("//li[text()='10 Minutes']")).Click();
-
             driver.FindElement(By.Id("postform-name")).SendKeys("how to gain dominance among developers");
-            driver.FindElement(By.XPath("//button[@class='btn -big']")).Click();            
-
-            IWebElement syntaxHighlighting = driver.FindElement(By.XPath("//a[text()='Bash']"));
-            IWebElement codeFirstLine = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[1]/div/span[1]"));
-            IWebElement codeSecondLine = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[2]/div/span[1]"));
-            IWebElement codeThirdLine = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[3]/div/span[1]"));
-
+            driver.FindElement(By.XPath("//button[@class='btn -big']")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            WebElement syntax = (WebElement)driver.FindElement(By.XPath("//a[text()='Bash']"));
+            WebElement codeFirstLine = (WebElement)driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[1]/div/span[1]"));
+            WebElement codeSecondLine = (WebElement)driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[2]/div/span[1]"));
+            WebElement codeThirdLine = (WebElement)driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[2]/ol/li[3]/div/span[1]"));
             Assert.AreEqual("how to gain dominance among developers - Pastebin.com", driver.Title);
-            Assert.AreEqual("Bash", syntaxHighlighting.Text);
+            Assert.AreEqual("Bash", syntax.Text);
             Assert.AreEqual("git config", codeFirstLine.Text);
             Assert.AreEqual("git reset", codeSecondLine.Text);
             Assert.AreEqual("git push", codeThirdLine.Text);
+            driver.Quit();
+        }
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(14);
+        [Test]
+        public void LibcatTest()
+        {
 
-            driver.Close();
+            IWebDriver ChromeBrowser;
+
+            ChromeBrowser = new ChromeDriver("C:\\driversselenium\\chrome");
+            ChromeBrowser.Manage().Window.Maximize();
+            ChromeBrowser.Navigate().GoToUrl("https://libcat.ru/");
+
+            IWebElement searchInput = ChromeBrowser.FindElement(By.XPath("//*[@id='tg-header']/div[2]/div/div/div/div/form/fieldset/input[1]"));
+            searchInput.SendKeys("Пенелопа Уорд");
+
+            IWebElement searchButton = ChromeBrowser.FindElement(By.XPath("//*[@id='tg-header']/div[2]/div/div/div/div/form/fieldset/button/i"));
+            searchButton.Click();
         }
     }
 }
